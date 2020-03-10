a5821f6dd122660092489ff0bb34e5b77cb053f5
geniusserg <danilovsergeydmitrievich@gmail.com>
2020-03-10 22:30:30 +0300

    tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,25 +4,37 @@
 #include <fstream>
 #include <iostream>
 #include "Manufacture.h"
+#define testprojects "/tmp/in2.txt"
+#define testemplyees "/tmp/in.txt"
+#define testresults "/tmp/out.txt"
 using namespace std;
-TEST(global, main){
-	cout << "start";
-	Manufacture HR;
+
+int write(const string& project, const string& employee){
 	ofstream file2;
-	file2.open("/tmp/in2.txt",ios_base::out); 
-	file2 <<  "proj 10000000\n";
+	file2.open(testprojects,ios_base::out); 
+	file2 <<  project;
 	file2.close();
-	HR.load_project_data("/tmp/in2.txt");
 	ofstream file;
-	file.open("/tmp/in.txt",ios_base::out); 
-	file <<  "1 RTRT Cleaner 90 90";
+	file.open(testemplyees,ios_base::out); 
+	file <<  employee;
 	file.close();
-	HR.load_data("/tmp/in.txt");
-	HR.print_statistic("/tmp/out.txt");
-	ifstream res("/tmp/out.txt");
+}
+
+string output(){
+	ifstream res(testresults);
 	string test;
 	getline(res, test);
-	string right;
-	right = "1  RTRT  Cleaner  8100";
-	EXPECT_STREQ(right.c_str(), test.c_str());
+	return test;	
+}
+
+
+
+TEST(global, main){
+	Manufacture HR;
+	write ("proj 10000000\n", "1 RTRT Cleaner 90 90\n");
+	HR.load_project_data(testprojects);
+	HR.load_data(testemplyees);
+	HR.print_statistic(testresults);
+	string right = "1  RTRT  Cleaner  8100";
+	EXPECT_STREQ(right.c_str(), output().c_str());
 }
